8b3e914a99528015dd51b6dfeb9f59d610d4d172
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-10-02 21:11:39 +0100

    Cleaned up document, removed some commentsgst

--- pet_shop.rb
+++ pet_shop.rb
@@ -4,11 +4,9 @@
 
 require('pry-byebug')
 
-
 def pet_shop_name(shop)
   return shop[:name]
 end
-
 
 
 def total_cash(shop)
@@ -16,17 +14,14 @@ def total_cash(shop)
 end
 
 
-
 def add_or_remove_cash(shop, amount)
   shop[:admin][:total_cash] += amount
 end
 
 
-
 def add_or_remove_cash(shop, amount)
   shop[:admin][:total_cash] += amount
 end 
-
 
 
 def pets_sold(pet_shop)
@@ -34,12 +29,9 @@ def pets_sold(pet_shop)
 end
 
 
-
 def increase_pets_sold(shop, sold)
   shop[:admin][:pets_sold] += sold
 end 
-
-
 
 
 def stock_count(shop)
@@ -47,16 +39,14 @@ def stock_count(shop)
 end 
 
 
-
-
 def pets_by_breed(pet_shop, breed) 
   pets_by_breed = []
+ 
   for pet in pet_shop[:pets]
     pets_by_breed << pet if pet[:breed] == breed
   end
   return pets_by_breed
 end
-
 
 
 def pets_by_breed(pet_shop, breed) 
@@ -66,35 +56,32 @@ def pets_by_breed(pet_shop, breed)
   end
   return pets_by_breed
 end
-#notes w Zjolt
 #this function should take in a breed, and check how many appearences there are in the pet list
 #check the input_breed against the breed of each pet
 
 
-
 def find_pet_by_name(pet_shop, name)
-                              # "Arthur"
-                              for pet in pet_shop[:pets]
-                                if ( name == pet[:name] )
-                                  return pet
-                                end
-                              end
-                              return nil 
-  #returning nil after the loop means it won't return the first pet name after loop checks. it will run through the whole loop and look for arthur, if it doesn't find arthur it returns nil instead.
-end
 
+  for pet in pet_shop[:pets]
+    if ( name == pet[:name] )
+      return pet
+    end
+  end
+  return nil 
+end
+#returning nil after the loop means it won't return the first pet after it loops through the array. If it doesn't find arthur it returns nil. 
 
 
 def find_pet_by_name(pet_shop, name) 
   for pet in pet_shop[:pets]
-      if ( name == pet[:name] ) #looking for 'Fred', no key value of fred
-        return pet # won't return nil at this point, will return first hash in array which is 'sir percy'
+      if ( name == pet[:name] ) 
+        return pet 
       end
   end
- return nil # this will return nil as expected by test, as it will not find fred. This is after loop runs and then returned. If not here, the test will fail. 
+ return nil 
 end
-
-
+#looking for 'Fred', no key value 'Fred'
+#will return first key value 'sir percy' if nil wasn't returned. Nil is expected so it passes. 
 
 
 def remove_pet_by_name(pet_shop, name)
@@ -107,12 +94,9 @@ def remove_pet_by_name(pet_shop, name)
 end
 
 
-
-
 def add_pet_to_stock(pet_shop, pet)
   pet_shop[:pets] << pet
 end
-
 
 
 def customer_pet_count(customer)
@@ -120,25 +104,24 @@ def customer_pet_count(customer)
 end
 
 
-
-def add_pet_to_customer(customer, pet)
-  return customer[:pets] << pet 
+def add_pet_to_customer(customer, new_pet)
+  return customer[:pets] << new_pet 
+  # return customer[:pets].push(new_pet)
 end
-# return customer[:pets].push(new_pet)
 
 
-#optional
+# OPTIONAL 
 
 
 def customer_can_afford_pet(customer, new_pet)
-  if customer[:cash] == new_pet[:price] 
+  if customer[:cash] >= new_pet[:price] 
     return new_pet
   else
     return false
   end
 end
+# unsure if > or >= or == is best. Used >=
 
-# > or >= or == ? 
 
 def customer_can_afford_pet(customer, new_pet)
   if customer[:cash] <= new_pet[:price]
@@ -148,12 +131,7 @@ def customer_can_afford_pet(customer, new_pet)
 end
 
 
-
-
-def sell_pet_to_customer(pet, customer)
-  customer[:pet] << pet_shop[:pets]
-end 
+# Couldn't manage to pass the last test.
 
 
 end
-
